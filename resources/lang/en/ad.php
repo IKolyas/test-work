37b9e14fa99bb866c4f6724dde2696a247093dc1
ik_sunweb <ik@sunweb.pro>
2022-07-16 17:04:57 +0500

    base-backend: Добавляет error response messages

---
+++ resources/lang/en/ad.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'response' => [
+        'error' => [
+            'empty_ad_list' => 'Список объявлений пуст!',
+            'not_found_ad' => 'Объявление не найдено!',
+        ]
+    ]
+];
